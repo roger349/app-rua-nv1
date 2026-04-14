78fa1508ddc5d590f7134f9742564d299944a2e9
roger <roger349@gmail.com>
2026-04-14 17:05:07 -0300

    tercer commit app-rua-nv1

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/app-rua-nv1">
       <App />
     </BrowserRouter>
   </React.StrictMode>
