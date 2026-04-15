b226cb887df6eda65a26796ad06acd9c6c3b7457
roger <you@example.com>
2026-04-14 21:55:29 -0300

    noveno commit app-rua-nv1

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,39 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Login from "@/pages/Login"
-import Register from "@/pages/Register"
-import Dashboard from "@/pages/Dashboard"
-import Users from '@/pages/Users'
-import ProtectedRoute from "@/components/ProtectedRoute"
-import "./App.css"
-import ResetPassword from './pages/ResetPassword'
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "@/pages/Login";
+import Register from "@/pages/Register";
+import Dashboard from "@/pages/Dashboard";
+import Users from "@/pages/Users";
+import ProtectedRoute from "@/components/ProtectedRoute";
+import ResetPassword from "@/pages/ResetPassword";
+import "./App.css";
 
 export default function App() {
-return (
-<div className="app-body w-screen h-screen">   
-   <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/ResetPassword" element={<ResetPassword />} />
-      <Route path="/dashboard/*" element={ <ProtectedRoute> <Dashboard /> </ProtectedRoute>}/>
-      <Route path="/users" element={<Users />} />
-      <Route path="*" element={<Navigate to="/login" />} />
-    </Routes>
-</div> 
-)
+  return (
+    <div className="app-body w-screen h-screen">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route
+          path="/dashboard/*"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/users"
+          element={
+            <ProtectedRoute>
+              <Users />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </div>
+  );
 }
 
 
