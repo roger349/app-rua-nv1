8f310623bbeabae89c6951b43bdbca42ca23c605
roger <you@example.com>
2026-04-14 23:25:30 -0300

    quinceavo commit app-rua-nv1

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import { navigate} from "react-router-dom";
 import { forgotPassword } from "../services/authService";
+import { useNavigate } from "react-router-dom";
 
 export default function ForgotPassword() {
   const [email, setEmail] = useState("");
   const [msg, setMsg] = useState("");
-
+  const navigate = useNavigate();
   const handleSubmit = async () => {
     try {
       await forgotPassword(email);
@@ -17,10 +17,10 @@ export default function ForgotPassword() {
 
   return (
      <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <form className="bg-slate-300 p-8 rounded-xl w-96 shadow">
-        <h2 className="text-xl font-bold mb-6 text-center">Recuperar contraseña </h2>
-        <input onChange={e => setEmail(e.target.value)} placeholder="Email" />
-        <button onClick={handleSubmit}>Enviar</button>
+      <form className="bg-gradient-to-r from-slate-300 to-slate-400 p-8 rounded-xl w-96 shadow">
+        <h1 className="text-xl font-bold mb-6 text-center">Recuperar contraseña </h1>
+        <input className="mt-4 w-full border border-fuchsia-700 hover:bg-blue-300 rounded p-2" onChange={e => setEmail(e.target.value)} placeholder="Email" />
+        <button className="mt-4 w-full border border-fuchsia-700 hover:bg-blue-300 rounded p-2" onClick={handleSubmit}>Enviar</button>
         <p>{msg}</p>   
         <button type="button" onClick={() => navigate("/login")} 
         className="mt-4 w-full border border-fuchsia-700 hover:bg-blue-300 rounded p-2">
