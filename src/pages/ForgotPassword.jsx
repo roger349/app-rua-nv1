37813748c2ffae516b2a9a2afff21cdbfc61362e
roger <you@example.com>
2026-04-14 23:10:58 -0300

    catorceavo commit app-rua-nv1

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { navigate} from "react-router-dom";
 import { forgotPassword } from "../services/authService";
 
 export default function ForgotPassword() {
@@ -10,19 +11,22 @@ export default function ForgotPassword() {
       await forgotPassword(email);
       setMsg("Revisa tu email");
     } catch {
-      setMsg("Error ❌");
+      setMsg("Error");
     }
   };
 
   return (
-    <div>
-      <h2>Recuperar contraseña</h2>
-
-      <input onChange={e => setEmail(e.target.value)} placeholder="Email" />
-
-      <button onClick={handleSubmit}>Enviar</button>
-
-      <p>{msg}</p>
+     <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <form className="bg-slate-300 p-8 rounded-xl w-96 shadow">
+        <h2 className="text-xl font-bold mb-6 text-center">Recuperar contraseña </h2>
+        <input onChange={e => setEmail(e.target.value)} placeholder="Email" />
+        <button onClick={handleSubmit}>Enviar</button>
+        <p>{msg}</p>   
+        <button type="button" onClick={() => navigate("/login")} 
+        className="mt-4 w-full border border-fuchsia-700 hover:bg-blue-300 rounded p-2">
+        Volver
+        </button>
+      </form>
     </div>
   );
 }
